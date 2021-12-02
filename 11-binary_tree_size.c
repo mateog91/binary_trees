a9f47a0d5cdc491527c99411e1bea542e2705710
Mateo <mateog91@gmail.com>
2021-12-02 09:21:16 -0500

    FIXED STYLE

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,9 +9,9 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    /* Base Case */
-    if (tree == NULL)
-        return (0);
-    /* Recursive Case */
-    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	/* Base Case */
+	if (tree == NULL)
+		return (0);
+	/* Recursive Case */
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
