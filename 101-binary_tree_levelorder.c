b7aee5ce9bfa104d6637109e362da180ea609d7d
Mateo <mateog91@gmail.com>
2021-12-02 23:57:30 -0500

    add 101

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -4,13 +4,14 @@
  * traversal
  *
  * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to print function
  *
  * Return: Nothing
  * If tree or func is NULL, do nothing
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-    /* Base Case */
-    if (tree == NULL || func == NULL)
-        return;
+	/* Base Case */
+	if (tree == NULL || func == NULL)
+		return;
 }
